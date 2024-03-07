b05f31c464c7cf591aa729ecf4259fbe923b4b19
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2024-03-07 10:52:59 +0100

    First roi

--- Transformation.py
+++ Transformation.py
@@ -4,9 +4,7 @@ from plantcv import plantcv as pcv
 class Transformation:
     def __init__(self, pathname: str, filter: str, debug: str) -> None:
         # Reading image
-        self.image = None
-        self.path = None
-        self.filename = None
+        self.image, self.path, self.filename = pcv.readimage(self.pathname)
         self.pathname = pathname
 
         # Debug
@@ -104,11 +102,64 @@ class Transformation:
         self._mask = masked
         return masked
 
+    def roi_border(self, image, border_width: int):
+        # HAS TO BE IMPROVED
+
+        image_width = image.shape[0]
+        image_height = image.shape[1]
+
+        for x in range(0, image_width):
+            for y in range(0, image_height):
+                if (
+                    (
+                        0 <= x <= image_width and
+                        0 <= y <= border_width
+                    ) or
+                    (
+                        0 <= x <= image_width and
+                        image_height - border_width <= y <= image_height
+                    ) or
+                    (
+                        0 <= x <= border_width and
+                        0 <= y <= image_height
+                    ) or
+                    (
+                        image_width - border_width <= x <= image_width and
+                        0 <= y <= image_height
+                    )
+                ):
+                    image[x, y] = (255, 0, 0)
+
+        return image
+
+
     def roi_objects(self):
-        roi1, roi_hierarchy = pcv.roi.rectangle(img=self._mask, x=0, y=0, h=256, w=256)
+        # HAS TO BE IMPROVED
+
+        roi = pcv.roi.rectangle(
+            img=self._plant_mask,
+            x=0,
+            y=0,
+            w=self.image.shape[0],
+            h=self.image.shape[1],
+        )
 
         if self.filter in ('all', 'roi_objects') and self.debug in ('plot', 'print'):
             pcv.params.debug = self.debug
+
+        kept_mask = pcv.roi.filter(
+            mask=self._plant_mask,
+            roi=roi,
+            roi_type='partial'
+        )
+
+        roi_image = self.image.copy()
+        roi_image[kept_mask != 0] = (0, 255, 0)
+        self.roi_border(roi_image, 5)
+
+        pcv.print_image(roi_image, 'roi_objects.png')
+
+        pcv.params.debug = None
 
         return None
 
