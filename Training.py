7b2c95f3455f5539259a7515768ff2a7b46b3add
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2024-03-15 14:23:50 +0100

    Creating my own model

--- Training.py
+++ Training.py
@@ -29,21 +29,16 @@ def build_model(number_classes):
     model.layers.append(layers.Rescaling(1.0 / 255))
 
     # Blocks
-    model.add(
-        layers.Conv2D(
-            filters=16, kernel_size=3, activation="relu", input_shape=(256, 256, 3)
-        )
-    )
-    model.add(layers.MaxPooling2D(pool_size=2))
-    model.add(layers.Conv2D(filters=32, kernel_size=3, activation="relu"))
-    model.add(layers.MaxPooling2D(pool_size=2))
-    model.add(layers.Conv2D(filters=64, kernel_size=3, activation="relu"))
-    model.add(layers.MaxPooling2D(pool_size=2))
+    model.add(layers.Conv2D(filters=32, kernel_size=(3, 3), activation="relu"))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(layers.Conv2D(filters=64, kernel_size=(3, 3), activation="relu"))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 
     # Layers
     model.add(layers.Flatten())
-    model.add(layers.Dense(128, activation="relu"))
-    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(64, activation="relu"))
+    model.add(layers.Dense(64, activation="relu"))
+    model.add(layers.Dropout(0.1))
     model.add(layers.Dense(number_classes, activation="softmax"))
 
     model.compile(
@@ -59,32 +54,25 @@ def main():
     if len(sys.argv) != 2:
         raise ValueError("usage: python Training.py [images folder]")
 
+    batch_size = 32
+
     # Dataset
     training_data, validation_data = preparing_dataset(sys.argv[1])
 
     # Model
     model = build_model(len(training_data.class_names))
+    model.build(input_shape=(None, 256, 256, 3))
 
     # Summary
     print(model.summary())
 
     # Training
-    early_stopping = callbacks.EarlyStopping(
-        monitor="val_loss", patience=5, restore_best_weights=True
-    )
-
-    history = model.fit(
+    model.fit(
         training_data,
         validation_data=validation_data,
         epochs=3,
-        callbacks=[early_stopping],
-        use_multiprocessing=True,
+        callbacks=[callbacks.EarlyStopping(monitor="val_loss", patience=10)],
     )
-    train_accuracy = history.history["accuracy"][-1]
-    validation_accuracy = history.history["val_accuracy"][-1]
-
-    print(f"Training Accuracy: {train_accuracy:.4f}")
-    print(f"Validation Accuracy: {validation_accuracy:.4f}")
 
     # Prediction
     print(model.predict(validation_data))
