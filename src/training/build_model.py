9fe674090dd91857e255dfc37e036ef881e5fe4d
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2024-03-20 20:21:11 +0100

    Best model so fare

--- src/training/build_model.py
+++ src/training/build_model.py
@@ -12,24 +12,23 @@ def build_model(training_data, summary=False):
     model.add(layers.Rescaling(1.0 / 255))
 
     # Blocks 1-4
-    for f in [32, 64, 128]:
+    for f in [32, 64, 64]:
         model.add(layers.Conv2D(filters=f, kernel_size=(3, 3), activation="relu"))
         model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+        model.add(layers.Dropout(0.1))
 
     # Flatten
     model.add(layers.Flatten())
 
     # Dense layers with dropout
-    model.add(
-        layers.Dense(128, activation="relu", kernel_regularizer=regularizers.l2(0.1))
-    )
+    model.add(layers.Dense(64, activation="relu"))
 
     # Output layer
     model.add(layers.Dense(number_classes, activation="softmax"))
 
     # Compile
     model.compile(
-        optimizer=Adam(0.001),
+        optimizer=Adam(0.0001),
         loss=losses.SparseCategoricalCrossentropy(),
         metrics=["accuracy"],
     )
